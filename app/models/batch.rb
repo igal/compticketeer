5e039c5cfb423221b322b8a8e0e5f4e6b9cf9044
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-24 17:12:41 -0700

    Improved Batch, added #create_tickets! and spec.

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -4,4 +4,15 @@ class Batch < ActiveRecord::Base
 
   validates_presence_of :ticket_kind_id
   validates_presence_of :emails
+
+  after_save :create_tickets!
+
+protected
+
+  # Create the tickets associated with this batch.
+  def create_tickets!
+    for email in self.emails.split("\n").map(&:strip)
+      self.tickets.create!(:email => email, :ticket_kind => self.ticket_kind)
+    end
+  end
 end
--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -6,4 +6,11 @@ describe Batch do
     attributes = Factory.attributes_for(:batch, :ticket_kind => kind)
     Batch.create!(attributes)
   end
+
+  it "should create associated tickets" do
+    batch = Factory(:batch, :emails => "foo@bar.com\nbaz@qux.org")
+    batch.tickets.size.should == 2
+    batch.tickets.map(&:email).should include("foo@bar.com")
+    batch.tickets.map(&:email).should include("baz@qux.org")
+  end
 end
