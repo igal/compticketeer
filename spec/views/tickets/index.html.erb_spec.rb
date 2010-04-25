7bb39624427e937ace34d82a200763b6d7ad3f73
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-25 03:30:12 -0700

    Updated tickets#index spec.

--- spec/views/tickets/index.html.erb_spec.rb
+++ spec/views/tickets/index.html.erb_spec.rb
@@ -5,29 +5,13 @@ describe "/tickets/index.html.erb" do
 
   before(:each) do
     assigns[:tickets] = [
-      stub_model(Ticket,
-        :ticket_kind_id => 1,
-        :batch_id => 1,
-        :email => "value for email",
-        :processed => false,
-        :error => "value for error"
-      ),
-      stub_model(Ticket,
-        :ticket_kind_id => 1,
-        :batch_id => 1,
-        :email => "value for email",
-        :processed => false,
-        :error => "value for error"
-      )
+      Factory.stub(:ticket, :email => "value for email", :error => "value for error"),
     ]
   end
 
   it "renders a list of tickets" do
     render
-    response.should have_tag("tr>td", 1.to_s, 2)
-    response.should have_tag("tr>td", 1.to_s, 2)
     response.should have_tag("tr>td", "value for email".to_s, 2)
-    response.should have_tag("tr>td", false.to_s, 2)
     response.should have_tag("tr>td", "value for error".to_s, 2)
   end
 end
