a615f6e4d65083b58fd85ff7452b841f433153af
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-25 02:50:41 -0700

    Improved TicketKind, added validations.

--- app/models/ticket_kind.rb
+++ app/models/ticket_kind.rb
@@ -2,4 +2,8 @@ class TicketKind < ActiveRecord::Base
   named_scope :ordered, :order => "title asc"
 
   has_many :tickets
+
+  validates_presence_of :title
+  validates_presence_of :prefix
+  validates_presence_of :template
 end
