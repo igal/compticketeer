0080128968b15ba0af358d84891ec3ac3c6793ac
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-24 17:11:03 -0700

    Improved Ticket model, added validations.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,8 @@
 class Ticket < ActiveRecord::Base
   belongs_to :batch
   belongs_to :ticket_kind
+
+  validates_presence_of :batch
+  validates_presence_of :ticket_kind
+  validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 end
