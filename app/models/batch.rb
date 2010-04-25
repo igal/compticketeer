05c91ce3f51529d7c61682d43448759c8fc6cab0
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-24 21:24:37 -0700

    Improved Batch model and spec: added #create_tickets and #validate_tickets callbacks.

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -4,15 +4,27 @@ class Batch < ActiveRecord::Base
 
   validates_presence_of :ticket_kind_id
   validates_presence_of :emails
+ 
+  before_validation :create_tickets
+  after_validation :validate_tickets
 
-  after_save :create_tickets!
-
-protected
+  protected
 
   # Create the tickets associated with this batch.
-  def create_tickets!
-    for email in self.emails.split("\n").map(&:strip)
-      self.tickets.create!(:email => email, :ticket_kind => self.ticket_kind)
+  def create_tickets
+    for email in self.emails.split(/\s+/).map(&:strip)
+      self.tickets << Ticket.new(:email => email, :ticket_kind => self.ticket_kind, :batch => self)
+    end
+  end
+
+  # Validate the associated tickets and add validation errors if needed.
+  def validate_tickets
+    if self.errors[:tickets]
+      self.errors.clear
+      for ticket in self.tickets
+        next if ticket.valid?
+        self.errors.add_to_base("Ticket with address '#{ticket.email}': " + ticket.errors.full_messages.join(', '))
+      end
     end
   end
 end
--- spec/models/batch_spec.rb
+++ spec/models/batch_spec.rb
@@ -4,13 +4,24 @@ describe Batch do
   it "should create a new instance given valid attributes" do
     kind = Factory(:ticket_kind)
     attributes = Factory.attributes_for(:batch, :ticket_kind => kind)
+
     Batch.create!(attributes)
   end
 
   it "should create associated tickets" do
     batch = Factory(:batch, :emails => "foo@bar.com\nbaz@qux.org")
+
     batch.tickets.size.should == 2
     batch.tickets.map(&:email).should include("foo@bar.com")
     batch.tickets.map(&:email).should include("baz@qux.org")
+  end
+
+  it "should not create record and report error if emails are invalid" do
+    batch = Factory.build(:batch, :emails => "foo@bar.com\nomgwtfbbq\n\r\n")
+
+    batch.should_not be_valid
+
+    batch.errors.size.should == 1
+    batch.errors.full_messages.first.should =~ /omgwtfbbq/
   end
 end
