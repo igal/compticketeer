e444e01d1c6288022fed521f0e5a6b18614380b5
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-24 21:43:05 -0700

    Fixed batches/show.html.erb_spec, assigned a batch.

--- spec/views/batches/show.html.erb_spec.rb
+++ spec/views/batches/show.html.erb_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "/batches/show.html.erb" do
   include BatchesHelper
   before(:each) do
-    assigns[:batch] = @batch = stub_model(Batch)
+    assigns[:batch] = @batch = Factory.stub(:batch)
   end
 
   it "renders attributes in <p>" do
