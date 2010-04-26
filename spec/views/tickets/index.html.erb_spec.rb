9ecf6f3318a715e0870ff0852f62fee25a3a963e
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-26 00:23:12 -0700

    Updated tickets#index view spec to use new behavior where ticket_kind is assigned on validation.

--- spec/views/tickets/index.html.erb_spec.rb
+++ spec/views/tickets/index.html.erb_spec.rb
@@ -5,13 +5,13 @@ describe "/tickets/index.html.erb" do
 
   before(:each) do
     assigns[:tickets] = [
-      Factory.stub(:ticket, :email => "value for email", :error => "value for error"),
+      Factory(:ticket, :email => "foo@bar.com", :error => "value for error"),
     ]
   end
 
   it "renders a list of tickets" do
     render
-    response.should have_tag("tr>td", "value for email".to_s, 2)
+    response.should have_tag("tr>td", "foo@bar.com".to_s, 2)
     response.should have_tag("tr>td", "value for error".to_s, 2)
   end
 end
