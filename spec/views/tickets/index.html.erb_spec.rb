130433952090b006daab32a2d3692b6d47f504a7
Igal Koshevoy <igal@pragmaticraft.com>
2010-04-26 04:34:48 -0700

    Updated tickets#indes view spec to remove :error column.

--- spec/views/tickets/index.html.erb_spec.rb
+++ spec/views/tickets/index.html.erb_spec.rb
@@ -5,13 +5,12 @@ describe "/tickets/index.html.erb" do
 
   before(:each) do
     assigns[:tickets] = [
-      Factory(:ticket, :email => "foo@bar.com", :error => "value for error"),
+      Factory(:ticket, :email => "foo@bar.com"),
     ]
   end
 
   it "renders a list of tickets" do
     render
     response.should have_tag("tr>td", "foo@bar.com".to_s, 2)
-    response.should have_tag("tr>td", "value for error".to_s, 2)
   end
 end
