33453e51ea7a2bdac4e7deee4d1648f193ea66ac
Igal Koshevoy <igal@pragmaticraft.com>
2010-05-02 15:23:15 -0700

    Enabled email delivery errors in all environments.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,8 +21,8 @@ config.action_view.cache_template_loading            = true
 # Enable serving of images, stylesheets, and javascripts from an asset server
 # config.action_controller.asset_host = "http://assets.example.com"
 
-# Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+# Enable delivery errors
+config.action_mailer.raise_delivery_errors = true
 
 # Enable threaded mode
 # config.threadsafe!
