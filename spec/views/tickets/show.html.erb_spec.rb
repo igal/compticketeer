12f35e35933eeea4d3bbe75c53869fecb521e1de
Kirsten Comandich <kcomandich@gmail.com>
2010-05-02 19:31:04 -0700

    Removed tickets_controller views and actions for edit, create, new.

--- spec/views/tickets/show.html.erb_spec.rb
+++ spec/views/tickets/show.html.erb_spec.rb
@@ -3,21 +3,12 @@ require 'spec_helper'
 describe "/tickets/show.html.erb" do
   include TicketsHelper
   before(:each) do
-    assigns[:ticket] = @ticket = stub_model(Ticket,
-      :ticket_kind_id => 1,
-      :batch_id => 1,
-      :email => "value for email",
-      :processed => false,
-      :error => "value for error"
-    )
+    assigns[:ticket] = @ticket = Factory(:ticket)
   end
 
   it "renders attributes in <p>" do
     render
-    response.should have_text(/1/)
-    response.should have_text(/1/)
-    response.should have_text(/value\ for\ email/)
-    response.should have_text(/false/)
-    response.should have_text(/value\ for\ error/)
+    response.should have_text(/#{@ticket.email}/)
+    response.should have_text(/#{@ticket.report}/)
   end
 end
