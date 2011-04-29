978e4bfe62fdd44942510ed7250dc5c316f46d24
Igal Koshevoy <igal@pragmaticraft.com>
2011-04-29 11:00:50 -0700

    Switch dependency management to Bundler.

---
+++ Gemfile
@@ -0,0 +1,11 @@
+gem 'rails', '2.3.10'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+gem 'aasm', '2.2.0'
+gem 'authlogic', '2.1.6'
+gem 'super_exception_notifier', '2.0.8', :require => 'exception_notifier'
+gem 'json', '1.5.1'
+
+gem 'factory_girl', '1.3.3', :require => false, :groups => ['development', 'test']
+gem 'rspec-rails',  '1.3.3', :require => false, :groups => ['development', 'test']
+gem 'ruby-debug', :groups => ['development', 'test'], :platforms => :ruby
--- config/environment.rb
+++ config/environment.rb
@@ -1,24 +1,12 @@
 # Be sure to restart your server when you modify this file
 
-# Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.10' unless defined? RAILS_GEM_VERSION
-
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'aasm', :version => '2.2.0'
-  config.gem 'authlogic', :version => '2.1.6'
-  config.gem 'super_exception_notifier', :version => '~> 2.0.8', :lib => 'exception_notifier'
-  config.gem 'json', :version => '1.5.1'
+  # See `Gemfile` for list of gem libraries used by this application.
 
-  if ['development', 'test'].include? RAILS_ENV
-    config.gem 'factory_girl', :version => '1.3.3', :lib => false
-    config.gem 'rspec-rails',  :version => '1.3.3', :lib => false
-  end
-
-  # Libraries
+  # Libraries loaded from Ruby itself
   require 'digest/md5'
 
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
